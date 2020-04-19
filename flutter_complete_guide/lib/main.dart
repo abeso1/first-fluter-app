9ebb960f9050ab4eab633bf7aea15b74a39b6908
Amer Beso <abeso1@etf.unsa.ba>
2020-04-19 15:24:38 +0200

    3 types of functions added

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer choosen');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,15 +23,17 @@ class MyApp extends StatelessWidget {
             Text('The question!'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: () => print('Answer 2 chosen!'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: (){
+                print("Answer 3 chooosen!!");
+              },
             ),
           ],
         ),
